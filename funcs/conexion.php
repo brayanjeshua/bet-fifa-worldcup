c9af6cc3b14054a9e4c958404f392987bb629293
Brayan Jeshua <brayanjeshuavz@gmail.com>
2022-05-12 17:21:20 +0200

    Conection in server

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	$mysqli=new mysqli("localhost","root","","pollera"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli=new mysqli("localhost:3306","pollera_user","!Ah4i4d5","pollera"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
